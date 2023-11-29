e758cb840b7f9b05da324b3b9b740515694f20eb
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-11-29 17:57:51 +0100

    Actualizacion Tecnologias

--- Client/src/Components/Technologies.jsx
+++ Client/src/Components/Technologies.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import node from "/src/assets/Icons/node.png";
+import javaScript from "/src/assets/Icons/js.png";
+import typeScript from "/src/assets/Icons/typescript.png";
+import react from "/src/assets/Icons/react.png";
+import angular from "/src/assets/Icons/angular.png";
+import express from "/src/assets/Icons/express.png";
+import redux from "/src/assets/Icons/redux.png";
+import tailwindCSS from "/src/assets/Icons/tailwind.png";
+import css from "/src/assets/Icons/css.png";
+import html from "/src/assets/Icons/html.png";
+import mySQL from "/src/assets/Icons/mysql.png";
+import postgre from "/src/assets/Icons/postgre.png";
 
 export default function Technologies () {
     return (
@@ -10,85 +22,85 @@ export default function Technologies () {
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/node.png"
-                                 alt="CSS"/>
+                                 src={node}
+                                 alt="Node.js"/>
                             <h3 className="text-zinc-300 font-jetbrains">Node.js</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/js.png"
-                                 alt="CSS"/>
+                                 src={javaScript}
+                                 alt="JavaScript"/>
                             <h3 className="text-zinc-300 font-jetbrains">JavaScript</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/typescript.png"
-                                 alt="CSS"/>
+                                 src={typeScript}
+                                 alt="TypeScript"/>
                             <h3 className="text-zinc-300 font-jetbrains">TypeScript</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/react.png"
-                                 alt="CSS"/>
+                                 src={react}
+                                 alt="React"/>
                             <h3 className="text-zinc-300 font-jetbrains">React</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/angular.png"
-                                 alt="CSS"/>
+                                 src={angular}
+                                 alt="Angular"/>
                             <h3 className="text-zinc-300 font-jetbrains">Angular</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/express.png"
-                                 alt="CSS"/>
+                                 src={express}
+                                 alt="Express"/>
                             <h3 className="text-zinc-300 font-jetbrains">Express</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/redux.png"
-                                 alt="CSS"/>
+                                 src={redux}
+                                 alt="Redux"/>
                             <h3 className="text-zinc-300 font-jetbrains">Redux</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/tailwind.png"
-                                 alt="CSS"/>
+                                 src={tailwindCSS}
+                                 alt="TailwindCSS"/>
                             <h3 className="text-zinc-300 font-jetbrains">TailwindCSS</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/css.png"
+                                 src={css}
                                  alt="CSS"/>
                             <h3 className="text-zinc-300 font-jetbrains">CSS</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/html.png"
-                                 alt="CSS"/>
+                                 src={html}
+                                 alt="HTML"/>
                             <h3 className="text-zinc-300 font-jetbrains">HTML</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/mysql.png"
-                                 alt="CSS"/>
+                                 src={mySQL}
+                                 alt="MySQL"/>
                             <h3 className="text-zinc-300 font-jetbrains">MySQL</h3>
                         </div>
 
                         <div className="flex flex-col items-center text-center border-2 border-zinc-300 rounded-xl m-2 py-1 px-4">
                             <img className="w-12"
-                                 src="/src/assets/Icons/postgre.png"
-                                 alt="CSS"/>
+                                 src={postgre}
+                                 alt="Postgre"/>
                             <h3 className="text-zinc-300 font-jetbrains">Postgre</h3>
                         </div>
 
